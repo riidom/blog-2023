b59f625e596a26ac047238c63cd7a7d857d65e15
riidom <dominik@riidom.eu>
2023-09-24 15:50:49 +0200

    rework header

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 <?=head_and_header('Index', '.');?>
 
   <details class="header__imprint">
-    <summary  tabindex="0">§&emsp;</summary>
+    <summary  tabindex="0">§&nbsp;</summary>
     <p>Email: blog-at-riidom-dot-eu</p>
     <p>RSS: <a href="https://riidom.eu/blog/feed.xml">Feed</a></p>
     <p>Socials:
       <a href="https://cohost.org/riidom" target="_blank">cohost</a>
       <a href="https://metalhead.club/@riidom" target="_blank">Mastodon</a>
-      <a href="https://kbin.social/u/riidom" target="_blank">Kbin</a>
     </p>
     <p><?=string('imprint')?></p>
   </details>
